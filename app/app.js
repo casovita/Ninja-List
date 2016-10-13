cfe4152551be368ba11e85c923ff1c06687768f2
Michael <casovita@gmail.com>
2016-10-13 16:38:33 +0300

    some buty

--- app/app.js
+++ app/app.js
@@ -1,29 +1,29 @@
-var app = angular.module('NinjaApp',['ngRoute','ngAnimate']);
+var app = angular.module('NinjaApp', ['ngRoute', 'ngAnimate']);
 
-app.config(['$routeProvider','$locationProvider',function($routeProvider,$locationProvider){
-    
+app.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
+
     $locationProvider.html5Mode(true);
-// Before App Runs
+    // Before App Runs
     $routeProvider
-        .when('/home',{
-        templateUrl:'views/home.html',
-        controller:'NinjaController'
-    })
-    .when('/contact',{
-        templateUrl:'views/contact.html',
-        controller:'ContactController'
-    })
-    .when('/contact-success',{
-        templateUrl:'views/contact-success.html',
-        controller:'ContactController'
-    })
-    .when('/directory',{
-        templateUrl:'views/directory.html',
-        controller: 'NinjaController'
-    }).otherwise({
-        redirectTo:'/home'
-    })
-    
+        .when('/home', {
+            templateUrl: 'views/home.html',
+            controller: 'NinjaController'
+        })
+        .when('/contact', {
+            templateUrl: 'views/contact.html',
+            controller: 'ContactController'
+        })
+        .when('/contact-success', {
+            templateUrl: 'views/contact-success.html',
+            controller: 'ContactController'
+        })
+        .when('/directory', {
+            templateUrl: 'views/directory.html',
+            controller: 'NinjaController'
+        }).otherwise({
+            redirectTo: '/home'
+        });
+
 }]);
 
 //app.run(function(){
@@ -32,65 +32,65 @@ app.config(['$routeProvider','$locationProvider',function($routeProvider,$locati
 //    
 //});
 
-app.directive('randomNinja',[function(){
-return{
-    restrict:'E',
-    scope: {
-        ninjas:'=',
-        title: '=',
-    },
-    templateUrl:'views/random.html',
-    transclude: true,
-    replace: true,
-    controller: function($scope){
-        $scope.random = Math.floor(Math.random()*4);
+app.directive('randomNinja', [function () {
+    return {
+        restrict: 'E',
+        scope: {
+            ninjas: '=',
+            title: '='
+        },
+        templateUrl: 'views/random.html',
+        transclude: true,
+        replace: true,
+        controller: function ($scope) {
+            $scope.random = Math.floor(Math.random() * 4);
+        }
     }
-}    
 }]);
 //Catch Routing Errors
-app.directive('error',function($rootScope){
-    return{
+app.directive('error', function ($rootScope) {
+    return {
         restrict: 'E',
         template: '<div style:"color:red" ng-show="isError">$routeChangeError</div>',
-        link: function(scope){
-            $rootScope.$on("$routeChangeError",function(){
+        link: function (scope) {
+            $rootScope.$on("$routeChangeError", function () {
                 scope.isError = true;
             })
         }
     }
 });
 //Main Controller
-app.controller('NinjaController',['$scope','$http',function($scope,$http){
-    
-    $scope.removeNinja = function(ninja){
+app.controller('NinjaController', ['$scope', '$http', function ($scope, $http) {
+    'use strict'
+    $scope.removeNinja = function (ninja) {
         var removedNinja = $scope.ninjas.indexOf(ninja);
-        $scope.ninjas.splice(removedNinja,1);
+        $scope.ninjas.splice(removedNinja, 1);
     };
-    
-    $scope.addNinja = function(){
-      $scope.ninjas.push({
-          name: $scope.newNinja.name,
-          belt: $scope.newNinja.belt,
-          rate: parseInt($scope.newNinja.rate),
-          thumb: "http://loremflickr.com/75/75/face",
-          avalibale: true
-      })  
-      $scope.newNinja = {};
+
+    $scope.addNinja = function () {
+        $scope.ninjas.push({
+            name: $scope.newNinja.name,
+            belt: $scope.newNinja.belt,
+            rate: parseInt($scope.newNinja.rate),
+            thumb: "http://loremflickr.com/75/75/face",
+            avalibale: true
+        });
+        $scope.newNinja = {};
     };
-    
-    $http.get('data/ninjas.json').success(function(data){
-        $scope.ninjas =data;      
-    })
-  
-    $scope.removeAll = function(){
+
+    $http.get('data/ninjas.json').success(function (data) {
+        $scope.ninjas = data;
+    });
+
+    $scope.removeAll = function () {
         $scope.ninjas = [];
     };
-    
+
 }]);
 
-app.controller('ContactController',['$scope','$location',function($scope,$location){
-    
-    $scope.sendMessage = function(){
-      $location.path('/contact-success');  
+app.controller('ContactController', ['$scope', '$location', function ($scope, $location) {
+    'use strict'
+    $scope.sendMessage = function () {
+        $location.path('/contact-success');
     };
 }]);
